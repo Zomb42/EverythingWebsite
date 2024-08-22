eaa9dfcd5bd2648c4ba2e39f2f50faa4486872fb
Zomb42 <dericks1real@gmail.com>
2024-08-21 21:57:29 -0400

    change

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById('openPdfButton').addEventListener('click', function() {
+    // Replace 'path/to/yourfile.pdf' with the actual path to your PDF file
+    window.open('resume.pdf', '_blank');
+  });
