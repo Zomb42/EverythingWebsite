3be0b4c6f03b0db4b9786348fe0ac39219cbc59b
Zomb42 <dericks1real@gmail.com>
2024-08-22 23:12:27 -0400

    pass

--- java.js
+++ java.js
@@ -3,3 +3,14 @@ document.getElementById('openPdfButton').addEventListener('click', function() {
     window.open('resume.pdf', '_blank');
   });
 
+function checkPassword() {
+  const correctPassword = 'abc'; // Replace with your actual password
+  const inputPassword = document.getElementById('password').value;
+  
+  if (inputPassword === correctPassword) {
+      document.getElementById('login').style.display = 'none';
+      document.getElementById('protected-content').style.display = 'block';
+  } else {
+      document.getElementById('error-message').style.display = 'block';
+  }
+}
